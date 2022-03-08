6d7a75c499124d676e746f5dbe1afb9b9bbf6c5d
Lenovo <Lenovo@ROHIT>
2022-03-08 09:31:18 +0530

    spring app

---
+++ SpringBootMvcProj02-App/src/main/java/com/rk/SpringBootMvcProj02AppApplication.java
@@ -0,0 +1,13 @@
+package com.rk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcProj02AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcProj02AppApplication.class, args);
+	}
+
+}
